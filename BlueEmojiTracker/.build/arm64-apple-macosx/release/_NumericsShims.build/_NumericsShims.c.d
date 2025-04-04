175646e99b6a086bba3dc1c504a361f7dd3bf23a
Mikhail <shishov.ok@gmail.com>
2025-04-04 19:05:39 +0400

    Добавил .gitignore и удалил билд-файлы из Git

--- BlueEmojiTracker/.build/arm64-apple-macosx/release/_NumericsShims.build/_NumericsShims.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/shishov/Desktop/all\ Cursor\ /insta\ 3.0\ gamer/BlueEmojiTracker/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/module.modulemap \
-  /Users/shishov/Desktop/all\ Cursor\ /insta\ 3.0\ gamer/BlueEmojiTracker/.build/checkouts/swift-numerics/Sources/_NumericsShims/_NumericsShims.c \
-  /Users/shishov/Desktop/all\ Cursor\ /insta\ 3.0\ gamer/BlueEmojiTracker/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/_NumericsShims.h
